4dd13a79bf87dbfbbc33a67e9769abafd9799278
William Coutinho <william.csouza@hotmail.com>
2023-09-20 11:31:21 -0300

    Inclusão de dependências no POM + classe SwaggerConfig

---
+++ src/main/java/coutinho/demo/jwt/security/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package coutinho.demo.jwt.security.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info().title("JWT Security - Demo")
+                        .description("Projeto com demonstração de uma API com recurso de segurança JWT")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Projeto com demonstração de uma API com recurso de segurança JWT")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
