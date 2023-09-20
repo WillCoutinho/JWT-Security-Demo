a8246de634b94254ef1b1b4e07c86f808dade11f
William Coutinho <william.csouza@hotmail.com>
2023-09-20 11:16:04 -0300

    Project basic structure

---
+++ src/main/java/coutinho/demo/jwt/security/DemoJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package coutinho.demo.jwt.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coutinho/demo/jwt/security/DemoJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package coutinho.demo.jwt.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
